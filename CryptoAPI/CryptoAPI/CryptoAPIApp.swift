d96bdcf8688476d85a29af39322dad70046f1140
Guilherme Lenzolari <103944988+guilenzolari@users.noreply.github.com>
2024-07-02 14:22:47 -0300

    starts xcode project

---
+++ CryptoAPI/CryptoAPI/CryptoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAPIApp.swift
+//  CryptoAPI
+//
+//  Created by Guilherme Ferreira Lenzolari on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
